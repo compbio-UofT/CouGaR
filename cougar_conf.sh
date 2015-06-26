5d59b72c1dbea4dbf78a98a6cc16a6553f5c0343
misko <misko@cs.toronto.edu>
2015-06-25 21:49:48 -0400

    fixes/updates

---
+++ cougar_conf.sh
@@ -0,0 +1,13 @@
+g=/dupa-filer/misko/cougar/CouGaR/
+s=/home/misko/apps/bin/samtools
+j=/usr/bin/java
+b=/filer/misko/bedtools-2.17.0/
+c=/data/misko/2013.04.12/cs2-4.3/cs2.exe
+cg=/usr/bin/cgquery
+gt=/usr/bin/gtdownload
+key=/dupa-filer/misko/tcga/cghub.key
+p=/filer/misko/picard/picard-tools-1.56/
+
+#need this for gurobi
+#export LD_LIBRARY_PATH=/home/buske/arch/sge6/lib:/home/buske/arch/sge6/lib::/dupa-filer/misko/gurobi/gurobi550/linux64/lib/:/dupa-filer/misko/gurobi/gurobi550/linux64/lib/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/dupa-filer/misko/gurobi/gurobi550/linux64/lib/
---
+++ pipeline/01_prepare_sample.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#check that COUGARD has been properly set
+if [ -z "$COUGARD" ]; then echo "Please set COUGAR directory COUGARD=path"; exit; fi
+if [ ! -d "$COUGARD" ] ; then echo "COUGARD enviornment variable is not set properly"; exit; fi
+
+#load the configuation 
+. $COUGARD/cougar_conf.sh
+
+
+
+if [ $# -ne 4 ]; then
+	echo $0 "ID_folder_name tumor_bam normal_bam ref[hg18/hg19]"
+	exit
+fi
+
+wd=$1
+tumor_bamfilename=$2
+normal_bamfilename=$3
+ref=$4
+
+if [ "$ref" == "hg18" ] ; then 
+	echo "is hg18"
+elif [ "$ref" == "hg19" ] ; then 
+	echo "is hg19"
+else
+	echo "Reference genome is unrecognized, please use either hg19 or hg18"
+	exit
+fi
+
+if [ -d $wd -a `ls $wd | wc -l | awk '{print $1}'` -ne 1 ]; then 
+	echo Directory exists! $wd
+	#exit
+fi
+
+mkdir -p $wd 
+pushd $wd
+	echo $ref > ref
+
+	bamfile=$tumor_bamfilename
+	echo $bamfile tumor
+	ln -s $bamfile tumor.bam
+	$s index tumor.bam
+	$s mpileup -q ${cov_mapq} tumor.bam | $g/getcov/get_cov tumor_cov 
+	sh $g/make_clusters.sh tumor.bam 0 &
+	sh $g/make_clusters.sh tumor.bam 15 &
+
+	bamfile=$normal_bamfilename
+	echo $bamfile normal
+	ln -s $bamfile normal.bam
+	$s index normal.bam
+	$s mpileup -q ${cov_mapq} normal.bam | $g/getcov/get_cov normal_cov 
+	sh $g/make_clusters.sh normal.bam 0 &
+	sh $g/make_clusters.sh normal.bam 15 &
+wait
+	rm tumor.bam
+	rm normal.bam
+popd
